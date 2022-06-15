1002fb178d0703e86b7242e65f7e8092a963cc6e
Mogamat-S23 <saleemariefdien@gmail.com>
2022-06-15 16:07:08 +0200

    new changes

---
+++ script/code.js
@@ -0,0 +1,26 @@
+// function calculate() {
+//     "use strict";
+//     var age = document.getElementById("age").value,
+//         result = age * 365;
+//     document.getElementById("Output").innerHTML = result;
+//     if (age === "") {
+//         document.getElementById("Output").innerHTML = "This Field Is Empty";
+//     } else if (amount < 0) {
+//         document.getElementById("Output").innerHTML = "This Field Is Not accepted Negative numbers";
+//     } else {
+//         document.getElementById("Output").innerHTML = result;
+//     }
+// }
+
+
+let calculate = document.getElementById('cal')
+let c = calculate
+
+c.addEventListener('click',() => {
+    let age = document.getElementById('age').value
+    let days = age*365;
+    document.getElementById('days').innerHTML = days;
+    let hours = document.getElementById('hours')
+    let minutes = document.getElementById('minutes')
+    let seconds = document.getElementById('days')
+})
