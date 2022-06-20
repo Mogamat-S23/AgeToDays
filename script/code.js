00e3d73023174e0f32819f5ed20e0e491d0365a2
Mogamat-S23 <saleemariefdien@gmail.com>
2022-06-20 12:03:48 +0200

    added clear button

--- script/code.js
+++ script/code.js
@@ -16,11 +16,21 @@
 let calculate = document.getElementById('cal')
 let c = calculate
 
-c.addEventListener('click',() => {
+c.addEventListener('click', () => {
     let age = document.getElementById('age').value
-    let days = age*365;
+    let days = age * 365;
     document.getElementById('days').innerHTML = days;
     let hours = document.getElementById('hours')
     let minutes = document.getElementById('minutes')
     let seconds = document.getElementById('days')
 })
+
+let btnClear = document.getElementById('clear');
+
+btnClear.addEventListener('click', () => {
+    let age = document.getElementById('age')
+    age.value = ''
+    document.getElementById('days').innerHTML = ''
+
+
+});
